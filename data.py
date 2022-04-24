81f7433335acde44ab0a6dadc3697701f60804a9
minyeamer <minyeamer@gmail.com>
2022-04-24 18:20:55 +0900

    trouble shooting with requesting

--- data.py
+++ data.py
@@ -47,22 +47,22 @@ class KakaoPlaceData(PlaceData):
 
     def __init__(self, data=dict(), df=pd.DataFrame()):
         super().__init__(data, df)
-        self.similr_index = self.make_similar_index() if len(df) else None
+        self.similr_index = self.make_similar_index()
 
 
-    def request_data(self, service_info: dict, local_info: dict, keyword=str()):
+    def request_data(self, service_info: dict, local_info: dict, keyword=str(), size=1):
         """
         카카오 API로부터 장소 정보를 요청하고 추가적인 정보를 스크래핑하는 메인 메소드
         키워드가 없을 경우 빅데이터를 기반으로 모든 장소에 대한 정보 요청
         향후 다른 플랫폼(네이버 등)에 대한 검색 기능 추가 시 해당 메소드의 범용성을 개선해 상위 클래스 메소드로 변환
         """
 
-        place_dict = {'places': list(), 'errors': list()}
+        place_dict = {'places': dict(), 'errors': dict()}
         place_list = [keyword] if keyword else self.make_place_list(local_info)
 
-        service_url = service_info['url']
-        headers = {"Authorization": service_info['key']}
-        size = min(service_info['size'],len(place_list)) if service_info['size'] else None
+        service_url = service_info['urls']['kakao_search']
+        headers = {"Authorization": 'KakaoAK '+service_info['keys']['kakao_rest']}
+        size = min(size,len(place_list)) if size else None
 
         service = Service(executable_path=ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service)
@@ -71,37 +71,46 @@ class KakaoPlaceData(PlaceData):
             response = requests.get(url=service_url, headers=headers,
                                     params={'query': place_name}).json()
             for place in response['documents']:
+                place_name = place.pop('place_name','')
+
+                if (place_name in place_dict['places'] or
+                    place_name in place_dict['errors']):
+                    continue
+
                 try:
                     if place['address_name'].__contains__(local_info['address']):
                         if place['category_group_name'] == '음식점':
                             place.update(self.request_details(driver, place['place_url']))
                             place.update(self.get_token_dict(
                                 place['category_name'], place['menu'], place['review']))
-                            place.update(self.get_sentiment_dict(place['review']))
-                            place_dict['places'].append(place)
-                except:
-                    place_dict['errors'].append(place)
+                            place.update(self.request_sentiment(service_info, place['review']))
+                            place_dict['places'][place_name] = place
+                except Exception as e:
+                    place['log'] = (type(e), e) # 에러 메시지 로그 기록
+                    place_dict['errors'][place_name] = place
 
         driver.close()
         self.update_data(place_dict)
-        self.update_dataframe(self.dict_to_df(place_dict))
+        self.update_dataframe(self.dict_to_df(place_dict['places']))
 
 
     def make_place_list(self, local_info: dict) -> list:
         """
-        빅데이터를 기반으로 서비스 지역 내 장소 목록을 반환하는 메소드
-        현재는 전국 인허가 음식점 빅데이터를 서버로부터 직접 가져와 수동으로 전처리 진행
+        전국 인허가 음식점 빅데이터를 기반으로 서비스 지역 내 장소 목록을 반환하는 메소드
+        용량 문제로 원본 파일 미첨부 @ https://www.localdata.go.kr/datafile/each/07_24_05_P_CSV.zip
         """
 
-        # 용량 문제로 원본 파일 미첨부 @ https://www.localdata.go.kr/datafile/each/07_24_05_P_CSV.zip
-        place_df = pd.read_csv('debugs/rest.csv', encoding='cp949')
+        place_df = pd.read_csv('data/rest.csv')
 
-        place_df = place_df[place_df['영업상태명'] != '폐업']
-        place_df = place_df[['도로명전체주소','사업장명']]
-        place_df = place_df[(place_df['도로명전체주소'].notnull()) &
-                            (place_df['도로명전체주소'].str.contains(local_info['si'])) &
-                            (place_df['도로명전체주소'].str.contains(local_info['gu'])) &
-                            (place_df['도로명전체주소'].str.contains(local_info['dong']))]
+        # 원본 파일 전처리 시 아래 코드 사용
+        # place_df = pd.read_csv('debugs/rest.csv', encoding='cp949')
+
+        # place_df = place_df[place_df['영업상태명'] != '폐업']
+        # place_df = place_df[['도로명전체주소','사업장명']]
+        # place_df = place_df[(place_df['도로명전체주소'].notnull()) &
+        #                     (place_df['도로명전체주소'].str.contains(local_info['si'])) &
+        #                     (place_df['도로명전체주소'].str.contains(local_info['gu'])) &
+        #                     (place_df['도로명전체주소'].str.contains(local_info['dong']))]
 
         return place_df['사업장명'].tolist()
 
@@ -120,14 +129,10 @@ class KakaoPlaceData(PlaceData):
         details = dict()
 
         driver.get(place_url)
-        content_xpath= '/html/body/div[2]/div[2]'
+        content_xpath= 'html' # '/html/body/div[2]/div[2]'
         self.wait_for_xpath(driver, content_xpath, delay=3)
 
-        details_summary = self.get_details_summary(driver)
-        details['bg_image'] = details_summary['bg_image']
-        details['raiting'] = details_summary['raiting']
-        details['review_num'] = details_summary['review_num']
-        details['blog_num'] = details_summary['blog_num']
+        details.update(self.get_details_summary(driver))
         details['menu'] = self.get_details_menu(driver)
         details['review'] = self.get_details_review(driver, details['review_num'])
 
@@ -165,7 +170,7 @@ class KakaoPlaceData(PlaceData):
             blog_div = driver.find_element_by_class_name('cont_review')
             summary['blog_num'] = int(blog_div.find_element_by_class_name('num_g').text)
         except:
-            summary['blog_num']
+            summary['blog_num'] = 0
         
         return summary
 
@@ -250,7 +255,7 @@ class KakaoPlaceData(PlaceData):
     # =================================================================================
 
 
-    def get_token_dict(self, category: str, menu: list, review: list) -> dict:
+    def get_token_dict(self, category: str, menus: list, reviews: list) -> dict:
         """
         분류, 메뉴, 리뷰 데이터를 토큰화하는 메소드
         """
@@ -259,8 +264,8 @@ class KakaoPlaceData(PlaceData):
 
         token_dict['category_token'] = ' '.join(
             [cat.replace(',',' ') for cat in category.split(' > ')[1:]])
-        token_dict['menu_token'] = self.get_tokenized_menu(' '.join(menu))
-        token_dict['review_token'] = self.get_tokenized_review(' '.join(review))
+        token_dict['menu_token'] = self.get_tokenized_menu(' '.join(menus))
+        token_dict['review_token'] = self.get_tokenized_review(' '.join(reviews))
 
         return token_dict
 
@@ -296,18 +301,24 @@ class KakaoPlaceData(PlaceData):
         return ' '.join(token_list)
 
 
+    # =================================================================================
+    # ================================= Analysis Part =================================
+    # =================================================================================
+
+
     def make_similar_index(self) -> np.ndarray:
         """
         분류, 메뉴, 리뷰에 대한 코사인 유사도 합을 반환하는 메소드
         """
 
-        if not len(self.df):
-            raise Exception('해당 객체가 요청에 적합한 데이터를 가지고 있지 않습니다.')
+        try:
+            category_similarity = self.get_cosine_similarity('분류명 토큰화') * 0.3
+            menu_similarity = self.get_cosine_similarity('메뉴 토큰화') * 0.5
+            review_similarity = self.get_cosine_similarity('리뷰 토큰화') * 1
+            similarity = category_similarity + menu_similarity + review_similarity
+        except:
+            similarity = np.zeros([len(self.df), len(self.df)])
 
-        category_similarity = self.get_cosine_similarity('분류명 토큰화') * 0.3
-        menu_similarity = self.get_cosine_similarity('메뉴 토큰화') * 0.5
-        review_similarity = self.get_cosine_similarity('리뷰 토큰화') * 1
-        similarity = category_similarity + menu_similarity + review_similarity
         return similarity.argsort()[:, ::-1]
 
 
@@ -317,6 +328,9 @@ class KakaoPlaceData(PlaceData):
         """
 
         tokenized_data = self.df[column].fillna('')
+
+        if not len(tokenized_data):
+            raise Exception('해당 객체가 요청에 적합한 데이터를 가지고 있지 않습니다.')
 
         if column in {'분류명 토큰화', '메뉴 토큰화'}:
             vectorizer = CountVectorizer(min_df=0, ngram_range=(1,2))
@@ -332,7 +346,7 @@ class KakaoPlaceData(PlaceData):
 
     def get_similar_places(self, result_df: pd.DataFrame, column: str, display: int) -> pd.DataFrame:
         """
-        특정 조건을 만족하는 행과 유사한 데이터프레임을 반환하는 메소드
+        코사인 유사도에 기반하여 특정 조건을 만족하는 행과 유사한 데이터프레임을 반환하는 메소드
         """
 
         place_value = result_df.iloc[0][column]
@@ -346,22 +360,42 @@ class KakaoPlaceData(PlaceData):
         return result_df.iloc[:display] if len(result_df) > display else result_df
 
 
+    def request_sentiment(self, service_info: dict, reviews: list) -> dict:
+        """
+        네이버 CLOVA Sentiment를 통해 리뷰의 감정을 분류하고 각 분류별 개수를 반환하는 메소드
+        """
+
+        sentiment_dict = {'review_sentiment': list()}
+
+        service_url = service_info['urls']['naver_clova']
+        client_id = service_info['keys']['naver_clova'][0]
+        client_secret = service_info['keys']['naver_clova'][1]
+
+        headers = {
+            'X-NCP-APIGW-API-KEY-ID': client_id,
+            'X-NCP-APIGW-API-KEY': client_secret,
+            'Content-Type': 'application/json'
+        }
+
+        for review in reviews:
+            try:
+                data = {'content': review}
+                response = requests.post(service_url, headers=headers,
+                                         data=json.dumps(data)).json()
+                sentiment = response['document']['sentiment']
+                sentiment_dict['review_sentiment'].append(sentiment)
+            except:
+                sentiment_dict['review_sentiment'].append('error')
+
+        sentiment_dict['positive'] = sentiment_dict['review_sentiment'].count('positive')
+        sentiment_dict['negative'] = sentiment_dict['review_sentiment'].count('negative')
+
+        return sentiment_dict
+
+
     # =================================================================================
     # =================================== Data Part ===================================
     # =================================================================================
-
-
-    def get_sentiment_dict(self, review: list) -> dict:
-        """
-        리뷰의 감정을 분류하고 각 분류별 개수를 반환하는 메소드
-        """
-
-        sentiment_dict = dict()
-
-        sentiment_dict['positive'] = 0
-        sentiment_dict['negative'] = 0
-
-        return sentiment_dict
 
 
     def dict_to_df(self, data: dict) -> pd.DataFrame:
@@ -369,10 +403,10 @@ class KakaoPlaceData(PlaceData):
         딕셔너리 형태의 스크래핑 결과를 데이터프레임으로 변환하는 메소드
         """
 
-        if not data['places']:
+        if not data:
             raise Exception('해당 객체가 요청에 적합한 데이터를 가지고 있지 않습니다.')
 
-        df = pd.DataFrame(data['places'])
+        df = pd.DataFrame(data).T
         df.drop(['category_group_code','category_group_name','distance','id'], axis=1, inplace=True)
 
         kr_dict = dict()
@@ -391,22 +425,19 @@ class KakaoPlaceData(PlaceData):
         kr_dict['category_token'] = '분류명 토큰화'
         kr_dict['menu_token'] = '메뉴 토큰화'
         kr_dict['review_token'] = '리뷰 토큰화'
+        kr_dict['review_sentiment'] = '리뷰 감정'
         kr_dict['positive'] = '긍정 리뷰 수'
         kr_dict['negative'] = '부정 리뷰 수'
 
-        return df.rename(columns=kr_dict)
+        df.rename(columns=kr_dict, inplace=True)
+        df.index.name = '식당명'
 
+        sorted_columns = ['분류명','별점','리뷰 수','긍정 리뷰 수','부정 리뷰 수','블로그 리뷰 수',
+                          '웹페이지 주소','이미지 주소','도로명 주소','지번 주소','전화번호','x','y',
+                          '메뉴','리뷰','리뷰 감정','분류명 토큰화','메뉴 토큰화','리뷰 토큰화']
+        df = df.reindex(columns=sorted_columns)
 
-    def update_data(self, data: dict):
-        """
-        딕셔너리를 업데이트하는 메소드
-        """
-
-        if not self.data:
-            self.data.update(data)
-        else:
-            self.data['places'].append(data['places'])
-            self.data['errors'].append(data['errors'])
+        return df.reset_index()
 
 
     def update_dataframe(self, df: pd.DataFrame):
@@ -415,8 +446,10 @@ class KakaoPlaceData(PlaceData):
         """
 
         self.df = self.df.append(df)
-        self.df['인기도'] = self.df['별점']**1.2 + np.log(self.df['리뷰 수'])
-        self.df.sort_values(by=['인기도','식당명'], ascending=[False,True], inplace=True)
+        self.df['인기도'] = (self.df['별점'] +
+                           ((self.df['리뷰 수']-self.df['부정 리뷰 수'])/(self.df['리뷰 수']+1))*5.0)
+        self.df.sort_values(by=['인기도','긍정 리뷰 수','별점','식당명'],
+                            ascending=[False,False,False,True], inplace=True)
         del self.df['인기도']
         self.df.drop_duplicates(['식당명'], inplace=True)
         self.df = self.df.set_index('식당명').reset_index()
